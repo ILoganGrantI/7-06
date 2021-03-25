997bf9fd534e95febeefed07931224576bd6a565
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-25 14:47:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let number = 15 
+let guess = 0
+
+while (guess != number) {
+  if (guess > number) {
+alert('Too big, try again')
+}
+guess = prompt('Guess a number inbetween 1 - 18')
+guess = parseInt(guess)
+  if (guess < number) {
+alert('Too small, guess again')
+}
+}
+  if (guess === number) {
+alert('You guessed it')
+}
